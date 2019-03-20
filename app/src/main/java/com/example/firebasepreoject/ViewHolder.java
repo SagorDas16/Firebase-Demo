4e85d84e30a8a4af1e841a0c1cf95cc698b6e6a9
DAS <das.sagor14@gmail.com>
2019-03-21 00:48:12 +0600

    Retrieve data

---
+++ app/src/main/java/com/example/firebasepreoject/ViewHolder.java
@@ -0,0 +1,34 @@
+package com.example.firebasepreoject;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    View mView;
+
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        mView = itemView;
+
+    }
+    public void setDetails(Context ctx,String title, String description, String image){
+
+        TextView mTitleTv = mView.findViewById(R.id.rTitleTv);
+        TextView mDetailTv = mView.findViewById(R.id.rDescriptionTv);
+        ImageView mImageTv = mView.findViewById(R.id.rImageView);
+
+        mTitleTv.setText(title);
+        mDetailTv.setText(description);
+        Picasso.get().load(image).into(mImageTv);
+
+
+    }
+}
